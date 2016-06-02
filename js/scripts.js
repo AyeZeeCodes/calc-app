3c740b21dba5acae6c1f8a0dbadf692889402931
April Peng and Alex Zerbach <alex.zerbach@gmail.com>
2016-06-01 19:59:23 -0700

    did not want to delete calculator.html so copied content to index.html. altered scripts.js for 4-form functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,48 @@ var divide = function(number1,number2) {
 
 
 $(function() {
-	$("#blanks form").submit(function(event) {
-		var number1 = parseInt($("input#number1").val());
+	$("#add form").submit(function(event) {
+		var number1 = parseInt($("input#add1").val());
 		//var calculate = $("#calculate").val();
-		var number2 = parseInt($("input#number2").val());
+		var number2 = parseInt($("input#add2").val());
 
 		var result = add(number1, number2);
+		alert(result);
+		$("#result").text(result);
+		$("#output").show();
+		event.preventDefault();
+	});
+
+	$("#subtract form").submit(function(event) {
+		var number1 = parseInt($("input#subtract1").val());
+		//var calculate = $("#calculate").val();
+		var number2 = parseInt($("input#subtract2").val());
+
+		var result = subtract(number1, number2);
+		alert(result);
+		$("#result").text(result);
+		$("#output").show();
+		event.preventDefault();
+	});
+
+	$("#multiply form").submit(function(event) {
+		var number1 = parseInt($("input#multiply1").val());
+		//var calculate = $("#calculate").val();
+		var number2 = parseInt($("input#multiply2").val());
+
+		var result = multiply(number1, number2);
+		alert(result);
+		$("#result").text(result);
+		$("#output").show();
+		event.preventDefault();
+	});
+
+	$("#divide form").submit(function(event) {
+		var number1 = parseInt($("input#divide1").val());
+		//var calculate = $("#calculate").val();
+		var number2 = parseInt($("input#divide2").val());
+
+		var result = divide(number1, number2);
 		alert(result);
 		$("#result").text(result);
 		$("#output").show();
