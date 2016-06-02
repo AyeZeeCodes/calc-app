ec61ef8e075d9ccb2e38d767c213c8b2226954c4
April Peng and Alex Zerbach <alex.zerbach@gmail.com>
2016-06-01 19:19:13 -0700

    alter calculator for form functionality. add all js and css standard forms. update scripts.js and styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
-var add= function (number1,number2) {
+var add = function(number1,number2) {
 	return number1 + number2;
 };
 
-var number1 = parseInt (prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+var subtract = function(number1, number2) {
+	return number1 - number2;
+};
+
+var multiply = function(number1,number2) {
+	return number1 * number2;
+};
+
+var divide = function(number1,number2) {
+	return number1 / number2;
+};
+
+
+$(function() {
+	$("#blanks form").submit(function(event) {
+		var number1 = parseInt($("input#number1").val());
+		//var calculate = $("#calculate").val();
+		var number2 = parseInt($("input#number2").val());
+
+		var result = add(number1, number2);
+		alert(result);
+		$("#result").text(result);
+		$("#output").show();
+		event.preventDefault();
+	});
+});
